f664e828e0b4a848d77e0fc0698bf565360b35e5
Nicolas Martins <nimartins.na1@gmail.com>
2024-05-30 18:51:57 -0300

    Finalizado a pagina de termos

---
+++ src/app/termos/page.tsx
@@ -0,0 +1,85 @@
+
+export default function Termos() {
+    return (
+        <main>
+            <head>
+                <h1>Termos de Uso</h1>
+            </head>
+            
+            <section>
+                <h2>1. Introdução</h2>
+                <p>Bem-vindo à plataforma Mr. Turtle. Ao acessar e utilizar nosso site, você concorda com os termos e condições estabelecidos abaixo. Por favor, leia atentamente estes Termos de Uso antes de utilizar nossos serviços.</p>
+            </section>
+
+            <section>
+                <h2>2. Definições</h2>
+                <p>
+                    <b>Usuário:</b> Qualquer pessoa que acessa e utiliza o site da Mr. Turtle.<br />
+                    <b>Plataforma:</b> O site da Mr. Turtle, incluindo todas as suas funcionalidades e serviços oferecidos.<br />
+                    <b>Doações:</b> Contribuições financeiras feitas pelos usuários para apoiar projetos de preservação dos oceanos.
+                </p>
+            </section>
+
+            <section>
+                <h2>3. Aceitação dos Termos</h2>
+                <p>Ao utilizar a plataforma, o usuário concorda com os Termos de Uso e com a nossa Política de Privacidade. Se você não concordar com qualquer parte dos termos, não deve utilizar a plataforma.</p>
+            </section>
+
+            <section>
+                <h2>4. Uso da Plataforma</h2>
+                <p>
+                    <b>Conta de Usuário:</b> Para utilizar certas funcionalidades da plataforma, o usuário deve criar uma conta fornecendo informações precisas e atualizadas.<br />
+                    <b>Responsabilidades do Usuário:</b> O usuário é responsável por manter a confidencialidade das informações de sua conta e por todas as atividades realizadas sob sua conta.
+                </p>
+            </section>
+
+            <section>
+                <h2>5. Doações</h2>
+                <p>
+                    <b>Finalidade das Doações:</b> As doações feitas através da plataforma são destinadas a projetos de preservação dos oceanos.<br />
+                    <b>Taxa de Administração:</b> 5% das doações são alocadas para o funcionamento e manutenção do site da Mr. Turtle. O restante (95%) é destinado diretamente aos projetos de preservação.<br />
+                    <b>Transparência:</b> Nos comprometemos a fornecer informações transparentes sobre o uso das doações e o impacto dos projetos apoiados.
+                </p>
+            </section>
+
+
+            <section>
+                <h2>6. Conteúdo da Plataforma</h2>
+                <p>
+                    <b>Propriedade Intelectual:</b> Todo o conteúdo da plataforma, incluindo textos, imagens, gráficos e logotipos, é de propriedade da Mr. Turtle e está protegido por leis de direitos autorais.<br />
+                    <b>Uso Permitido:</b> O usuário pode acessar e utilizar o conteúdo da plataforma para fins pessoais e não comerciais. Qualquer uso não autorizado do conteúdo é proibido.
+                </p>
+            </section>
+
+            <section>
+                <h2>7. Pontos de Coleta e Denúncias Ambientais</h2>
+                <p>
+                    <b>Pontos de Coleta:</b> A plataforma fornece informações sobre pontos de coleta de lixo. Esses pontos são mantidos por terceiros, e a Mr. Turtle não se responsabiliza pela precisão dessas informações.<br />
+                    <b>Denúncias Ambientais:</b> O usuário pode utilizar a plataforma para fazer denúncias ambientais. A Mr. Turtle se compromete a encaminhar essas denúncias às autoridades competentes.
+                </p>
+            </section>
+
+            <section>
+                <h2>8. Modificações nos Termos</h2>
+                <p>A Mr. Turtle se reserva o direito de modificar estes Termos de Uso a qualquer momento. As mudanças entrarão em vigor imediatamente após a publicação no site. É responsabilidade do usuário revisar os Termos de Uso periodicamente.</p>
+            </section>
+
+            <section>
+                <h2>9. Limitação de Responsabilidade</h2>
+                <p>A Mr. Turtle não será responsável por quaisquer danos diretos, indiretos, incidentais, especiais ou consequenciais decorrentes do uso ou da incapacidade de uso da plataforma.</p>
+            </section>
+
+            <section>
+                <h2>10. Contato</h2>
+                <p>Para qualquer dúvida ou preocupação relacionada aos Termos de Uso, entre em contato pelo email fiapentregas@gmail.com</p>
+            </section>
+
+            <section>
+                <h2>11. Disposições Gerais</h2>
+                <p>Estes Termos de Uso constituem o acordo integral entre o usuário e a Mr. Turtle em relação ao uso da plataforma. Se qualquer disposição destes termos for considerada inválida, as demais disposições continuarão em pleno vigor e efeito.</p>
+
+                <p>Ao utilizar a plataforma Mr. Turtle, você concorda com todos os termos e condições acima estabelecidos. Agradecemos por apoiar nossa missão de preservar os oceanos.</p>
+            </section>
+        </main>
+    );
+}
