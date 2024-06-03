3555c052d0fe8cd369a16c344d3b1b64632d9c48
Yago Lucas Gonçalves da Silva <120147089+yagoluucas@users.noreply.github.com>
2024-06-03 19:59:15 -0300

    Atualizado para poder subir a aplicação

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useRouter } from "next/navigation";
 import LoginDiv from "@/components/Login";
-import { useEffect } from from "react";
+import { useEffect } from "react";
 
 export default function Login() {
     const rota = useRouter()
